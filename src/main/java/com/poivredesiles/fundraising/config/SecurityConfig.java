7d7c0c8e43c00f2a098885154e429fef8d88d853
daxyonis <emaciejko@gmail.com>
2021-02-03 06:53:18 -0500

    Login page WIP

--- src/main/java/com/poivredesiles/fundraising/config/SecurityConfig.java
+++ src/main/java/com/poivredesiles/fundraising/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.poivredesiles.fundraising.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,6 +12,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	// The public endpoints
+	private static final String[] PUBLIC = new String[]{"/error", "/login", "/logout"};
 
 	// Provide a bean for the password encoder
 	@Bean
@@ -21,12 +25,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+        	.antMatchers(PUBLIC).permitAll()
             .antMatchers("/").hasAnyAuthority("BUYER", "SELLER", "LEAD", "ADMIN")            
             .anyRequest().authenticated()
             .and()
-            .formLogin().permitAll()
+            .formLogin()
+            		.loginPage("/login")            		            	
             .and()
-            .logout().permitAll();            
+            .logout()           
+            	.logoutUrl("/logout")
+            	.logoutSuccessUrl("/login?logged-out");
     }
 	
+	@Override
+	public void configure(WebSecurity web) {
+		web.ignoring().antMatchers("/static/**", "/js/**", "/css/**","/image/**", "/favicon.ico","/webjars/**");
+	}
 }
--- src/main/java/com/poivredesiles/fundraising/controller/HomeController.java
+++ src/main/java/com/poivredesiles/fundraising/controller/HomeController.java
@@ -1,13 +1,18 @@
 package com.poivredesiles.fundraising.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String home() {
 		return "index";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "views/login";
 	}
 }
