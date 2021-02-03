063510bb58bc8b1d9fb498591afd68276e7f4a22
daxyonis <emaciejko@gmail.com>
2021-02-03 16:02:54 -0500

    Footer

--- src/main/java/com/poivredesiles/fundraising/controller/HomeController.java
+++ src/main/java/com/poivredesiles/fundraising/controller/HomeController.java
@@ -1,18 +1,42 @@
 package com.poivredesiles.fundraising.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
-public class HomeController {
+@PropertySource("classpath:git.properties")
 
+public class HomeController {
+	
+	private final Logger log = LoggerFactory.getLogger(HomeController.class);
+
+	@Value("${git.commit.id.abbrev}")
+	private String gitCommitId;	
+	
+	@Value("${git.build.version}")
+	private String buildVersion; 
+	
+	@ModelAttribute
+	public void populateModel(Model model) {
+		model.addAttribute("gitCommitId", gitCommitId);
+		model.addAttribute("buildVersion", buildVersion);
+	}
+	
 	@GetMapping("/")
 	public String home() {
+		log.info("Requested Home Page");
 		return "index";
 	}
 	
 	@GetMapping("/login")
 	public String login() {
+		log.info("Requested Login Page");
 		return "views/login";
 	}
 }
