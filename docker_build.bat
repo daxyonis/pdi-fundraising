683deb5057e71044e4bd5da56c0fc9a8a4aca34b
Eva Maciejko <emaciejko@gmail.com>
2026-01-22 10:36:24 -0500

    Fix docker build details

--- docker_build.bat
+++ docker_build.bat
@@ -1,1 +1,5 @@
-./mvnw spring-boot:build-image
+REM This command requires a buildpack image that doesn't exist for Spring Boot 4.0 yet
+REM ./mvnw spring-boot:build-image
+
+REM So we use our old fabric8 plugin instead
+./mvnw clean package docker:build
